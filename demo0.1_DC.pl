e80d6fca04f5832bdee3f4437d89640114508fb0
HuynhDuc <16520240@gm.uit.edu.vn>
2018-10-15 10:43:00 +0700

    [15102018] debug double show and have_133

--- demo0.1_DC.pl
+++ demo0.1_DC.pl
@@ -1,7 +1,7 @@
 start:-
     reason(Problem),
     write('I believe that the Computer have'),
-    write(Prolem),
+    write(Problem),
     nl,
     undo.
 /* reason that should be tested*/
@@ -10,38 +10,38 @@ reason(bo_nguon_hong):- bo_nguon_hong,!.
 reason(loi_bo_mach_chu):- loi_bo_mach_chu,!.
 reason(long_o_cung):- long_o_cung,!.
 reason(o_cung_hong_hoac_bi_bad):- o_cung_hong_hoac_bi_bad,!.
-reason(he_dieu_hanh_loi):- he_dieu_hanh_loi,!.
+reason(os_error):- os_error,!.
 reason(unknown). /*no diagnosis*/
 
 /*reason Identification Rules*/
-long_day_nguon:- 
+long_day_nguon:-
     verify(long_day_nguon),
     verify(den_nguon_tat_quat_khong_chay),
     write('De nghi kiem tra day nguon'),
     nl.
-bo_nguon_hong:- 
-    no(verify(long_day_nguon)),
+bo_nguon_hong:-
+    not(verify(long_day_nguon)),
     verify(den_nguon_tat_quat_khong_chay),
     write('Bo nguon hong, hay di thay nguon'),
     nl.
 loi_bo_mach_chu:-
     verify(quat_quay_nhung_khong_len_hinh),
-    verify(den_nguon_sang_co_tieng_bip_keu_lien_tuc),
+    verify(den_nguon_sang_co_tieng_bip),
     write('Loi bo mach chu, nen cai lai driver'),
     nl.
-long_o_cung:- 
+long_o_cung:-
     verify(long_o_cung),
     verify(den_o_cung_khong_doc),
     verify(den_nguon_sang_nhung_khong_nhan_os),
     write('long_o_cung'),
     nl.
 o_cung_hong_hoac_bi_bad:-
-    no(verify(long_o_cung)),
+    not(verify(long_o_cung)),
     verify(den_o_cung_khong_doc),
     verify(den_nguon_sang_nhung_khong_nhan_os),
     write('o cung hong hoac bi bad'),
     nl.
-he_dieu_hanh_loi:-
+os_error:-
     verify(den_o_cung_doc),
     verify(den_nguon_sang_nhung_khong_nhan_os),
     write('he dieu hanh loi, de nghi cai lai os'),
@@ -53,10 +53,10 @@ ask(Question):-
     write('? '),
     read(Response),
     nl,
-    ( (Response == yes ; Response == y) 
-    -> 
+    ( (Response == yes ; Response == y)
+    ->
     assert(yes(Question));
-    assert(nos(Question)), fail).
+    assert(no(Question)), fail).
 :- dynamic yes/1,no/1.
 verify(S):-
     (yes(S) -> true;
