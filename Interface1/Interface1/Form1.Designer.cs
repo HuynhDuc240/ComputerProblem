065bf21b5a6e849da556dcebaa5d535536b8cebf
leavin296 <leavin296@gmail.com>
2018-10-16 23:41:54 +0700

    create checkbox and output event to file

--- Interface1/Interface1/Form1.Designer.cs
+++ Interface1/Interface1/Form1.Designer.cs
@@ -36,10 +36,12 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.logo = new System.Windows.Forms.Label();
             this.headPanel = new System.Windows.Forms.Panel();
+            this.minimize = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.labelOfOption1 = new System.Windows.Forms.Label();
-            this.close = new System.Windows.Forms.Button();
-            this.minimize = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.optionPanel.SuspendLayout();
             this.iconPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -174,6 +176,26 @@
             this.headPanel.Size = new System.Drawing.Size(660, 89);
             this.headPanel.TabIndex = 1;
             // 
+            // minimize
+            // 
+            this.minimize.Image = ((System.Drawing.Image)(resources.GetObject("minimize.Image")));
+            this.minimize.Location = new System.Drawing.Point(603, 0);
+            this.minimize.Name = "minimize";
+            this.minimize.Size = new System.Drawing.Size(30, 30);
+            this.minimize.TabIndex = 7;
+            this.minimize.UseVisualStyleBackColor = true;
+            this.minimize.Click += new System.EventHandler(this.minimize_Click);
+            // 
+            // close
+            // 
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.Location = new System.Drawing.Point(630, 0);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(30, 30);
+            this.close.TabIndex = 6;
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
@@ -192,33 +214,36 @@
             this.labelOfOption1.Size = new System.Drawing.Size(245, 43);
             this.labelOfOption1.TabIndex = 4;
             this.labelOfOption1.Text = " Diagnose Problem";
-            this.labelOfOption1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // close
+            // panel1
             // 
-            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
-            this.close.Location = new System.Drawing.Point(630, 0);
-            this.close.Name = "close";
-            this.close.Size = new System.Drawing.Size(30, 30);
-            this.close.TabIndex = 6;
-            this.close.UseVisualStyleBackColor = true;
-            this.close.Click += new System.EventHandler(this.close_Click);
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(196, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(654, 400);
+            this.panel1.TabIndex = 2;
             // 
-            // minimize
+            // button1
             // 
-            this.minimize.Image = ((System.Drawing.Image)(resources.GetObject("minimize.Image")));
-            this.minimize.Location = new System.Drawing.Point(603, 0);
-            this.minimize.Name = "minimize";
-            this.minimize.Size = new System.Drawing.Size(30, 30);
-            this.minimize.TabIndex = 7;
-            this.minimize.UseVisualStyleBackColor = true;
-            this.minimize.Click += new System.EventHandler(this.minimize_Click);
+            this.button1.AutoSize = true;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Location = new System.Drawing.Point(567, 521);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "CHUẨN ĐOÁN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(850, 584);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.headPanel);
             this.Controls.Add(this.optionPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -233,6 +258,7 @@
             this.headPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -249,6 +275,8 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Button minimize;
         private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Interface1/Interface1/Form1.cs
+++ Interface1/Interface1/Form1.cs
@@ -7,28 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 namespace Interface1
 {
     public partial class home : Form
     {
-        
-    
+
+
         public home()
         {
             InitializeComponent();
+            addCheckBoxs();
+        }
+        string filePath = "G:/C_Sharp/Winform/Form/ComputerProblem/Interface1/FileStream/";
+        private Dictionary<string, KeyValuePair<string, bool>> symptoms = new Dictionary<string, KeyValuePair<string, bool>>()
+        {
+                { "Đèn nguồn tắt" , new KeyValuePair<string, bool>("led_on", false)},
+                { "Quạt tản nhiệt tắt", new KeyValuePair<string,bool>("fan_on", false) },
+                { "Lỏng dây nguồn", new KeyValuePair<string, bool>("loose_wire", false)},
+                { "Máy phát ra tiếng bip từng hồi và liên tục", new KeyValuePair<string,bool>("long_beep",false) },
+                { "Không vào được hệ điều hành", new KeyValuePair<string,bool>("not_load_os", false) },
+                { "Đèn ổ cứng tắt", new KeyValuePair<string, bool>("led_hard_drive_off", false) },
+                { "Ổ cứng bẩn", new KeyValuePair<string, bool>("dirty_hard_drive", false) },
+                { "Màn hình máy tính không hiển thị", new KeyValuePair<string, bool>("black_screen", false) },
+        };
+
+        private void addCheckBoxs()
+        {
+            int distanceCBox = 30;
+            foreach (KeyValuePair<string, KeyValuePair<string, bool>> sym in symptoms)
+            {
+                CheckBox cb = new CheckBox()
+                {
+                    Location = new Point(100, distanceCBox),
+                    Text = sym.Key,
+                    AutoSize = true,
+                    Font = new Font("Comic Sans MS", 15, FontStyle.Italic),
+                };
+                cb.CheckStateChanged += Cb_CheckStateChanged;
+                distanceCBox += 40;
+                panel1.Controls.Add(cb);
+            }
+        }
+
+        private void Cb_CheckStateChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            symptoms[cb.Text] = new KeyValuePair<string, bool>(symptoms[cb.Text].Key, !symptoms[cb.Text].Value);
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
-       
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void close_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -43,9 +74,16 @@ namespace Interface1
         {
             Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\Program Files\swipl");
             Environment.SetEnvironmentVariable("Path", Environment.GetEnvironmentVariable("Path") + @";C:\Program Files\swipl\bin");
-            
+
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (KeyValuePair<string, KeyValuePair<string, bool>> sym in symptoms)
+            {
+                string value = sym.Value.Value ? "yes." : "no.";
+                File.WriteAllText(filePath + sym.Value.Key, value);
+            }
+        }
     }
 }
